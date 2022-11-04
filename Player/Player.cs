1509b083f4089003ac50d284b5738902935f9f02
BAUHAUS <patrickfromgnomedk@gmail.com>
2022-11-04 20:48:12 +0100

    Player Movement Animation + Input Maps

--- Player/Player.cs
+++ Player/Player.cs
@@ -7,10 +7,13 @@ public class Player : KinematicBody2D
     public bool DebuggingEnabled;
 
     [Export]
+    public NodePath AnimatedSpritePath;
+    private AnimatedSprite animatedSprite;
+
+    [Export]
     public int MaxSpeed = 80;
     [Export]
     public int MaxRunSpeed = 80;
-
     [Export]
     public int Acceleration = 500;
     [Export]
@@ -21,6 +24,11 @@ public class Player : KinematicBody2D
     private Vector2 velocity;
     private Vector2 inputAxis;
 
+    public override void _Ready()
+    {
+        animatedSprite = GetNode<AnimatedSprite>(AnimatedSpritePath);
+    }
+
     public override void _PhysicsProcess(float delta) {
         PlayerMovement(delta);
     }
@@ -30,7 +38,7 @@ public class Player : KinematicBody2D
     }
 
     private void PlayerMovement(float delta) {
-        inputAxis = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        inputAxis = Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown");
         inputAxis = inputAxis.Normalized();
 
         if (Input.IsActionPressed("Run"))
@@ -40,11 +48,29 @@ public class Player : KinematicBody2D
 
         if (inputAxis != Vector2.Zero) {
             velocity = velocity.MoveToward(inputAxis * (MaxSpeed + runSpeed), Acceleration * delta);
+            animatedSprite.Animation = "Walk";
+            AnimationRotation();
         } else {
             velocity = velocity.MoveToward(Vector2.Zero, Friction * delta);
+            animatedSprite.Animation = "Idle";
         }
 
         velocity = MoveAndSlide(velocity);
+    }
+
+    // Lazy, just did this because of the Sprites we are currently using (There's probably another way to do it with the current sprites,
+    // but I don't know what that could be atm)
+    private void AnimationRotation()
+    {
+        if (inputAxis.y < 0)
+            animatedSprite.RotationDegrees = -90;
+        if (inputAxis.y > 0)
+            animatedSprite.RotationDegrees = 90;
+
+        if (inputAxis.x > 0)
+            animatedSprite.RotationDegrees = 0;
+        if (inputAxis.x < 0)
+            animatedSprite.RotationDegrees = -180;
     }
 
     private void Debugging() {
